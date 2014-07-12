7194f77d89c9711ec7f0661118dff9a2c06fa19b
cacherize <web.developing.maniac@gmail.com>
2014-07-12 03:13:40 -0500

    fixed user validations and removed password digest validation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,20 @@ class User < ActiveRecord::Base
 
   validates :first, presence: {message: 'Name must be given'}
   validates :last, presence: {message: 'Name must be given'}
-  validates :email, presence: {message: 'must be provided'}, uniqueness: {case_sensitive: false}, on: :create
+  validates :email, presence: {message: 'must be given'}, uniqueness: {case_sensitive: false}, on: :create
+  validates :password, presence: {message: 'must be given'}, on: :create
   validates :username, presence: {message: 'must be given'},
     uniqueness: {case_sensitive: false}, 
     length: {minimum: 3, maximum: 30,
       too_long:  "is too long (max 30 chars)",
-      too_short: "is too short (min 3 chars)"
+      too_short: "is too short (min 3 chars)",
+      if: lambda{self.username.present?}
     },
     format: {
       with: /^[a-z0-9_-]*$/i,
       message: 'must only contain alphanumeric characters',
       if: lambda{self.username.present?}
     }
+
+    after_validation { self.errors.messages.delete(:password_digest) }
 end
