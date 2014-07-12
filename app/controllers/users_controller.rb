d6673a84b69b44c993c8905036bf9e3c99495ce4
cacherize <web.developing.maniac@gmail.com>
2014-07-12 02:18:51 -0500

    more form styling and added user create action logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params[:user])
+    respond_to do |format|
+      if @user.save
+        format.html{redirect_to root_path, "Success! Please check your inbox for an account confirmation email. Confirm your account to login."}
+      else
+        format.html{render :create}
+      end
+    end
   end
 
   def edit
