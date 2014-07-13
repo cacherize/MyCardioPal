3e59b5d9dcecd1a7194209bd3db9bf647a556ca8
cacherize <web.developing.maniac@gmail.com>
2014-07-13 00:08:41 -0500

    fixed edit action user variable on password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @user.find_by_password_reset_token!(params[:id])
+    @user = User.find_by_password_reset_token!(params[:id])
   end
 
   def update
