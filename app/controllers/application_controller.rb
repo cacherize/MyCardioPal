12248c88627f7a17c70f53a75f9c8b058c7fdc45
cacherize <web.developing.maniac@gmail.com>
2014-07-13 23:34:46 -0500

    added before filter to redirect to the under development page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :store_location
   before_filter :authenticate
+  before_filter :redirect_to_under_dev_page
 
   private
+
+  def redirect_to_under_dev_page
+    if Rails.env.production?
+      redirect_to under_development_path unless request.remote_ip == "209.182.121.64"
+    end
+  end
 
   def store_location
     session["return_path"] = request.fullpath
--- app/controllers/under_development_controller.rb
+++ app/controllers/under_development_controller.rb
@@ -1,6 +1,7 @@
 class UnderDevelopmentController < ApplicationController
   skip_filter :store_location
   skip_filter :authenticate
+  skip_filter :redirect_to_under_dev_page
   def index
     respond_to do |format|
       format.html{render layout: 'blank_layout'}
