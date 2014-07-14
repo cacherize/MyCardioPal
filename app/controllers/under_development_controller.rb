7090eea77aed7c858d0fdf5df1c1cac3859bfda4
cacherize <web.developing.maniac@gmail.com>
2014-07-13 23:27:46 -0500

    added under development page

--- app/controllers/under_development_controller.rb
+++ app/controllers/under_development_controller.rb
@@ -2,5 +2,8 @@ class UnderDevelopmentController < ApplicationController
   skip_filter :store_location
   skip_filter :authenticate
   def index
+    respond_to do |format|
+      format.html{render layout: 'blank_layout'}
+    end
   end
 end
