6455832691769f90992da01e81f5072cbf001817
cacherize <web.developing.maniac@gmail.com>
2014-07-18 21:51:22 -0500

    added setter method for the time attribute on workouts

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,10 @@
 class Workout < ActiveRecord::Base
   attr_accessible :user_id, :activity_id, :time, :date, :distance, :incline, :note
+
+  def time=(values)
+    hours = values[:hours].to_i * 3600
+    minutes = values[:minutes].to_i * 60
+    seconds = values[:seconds].to_i
+    self[:time] = hours + minutes + seconds
+  end
 end
