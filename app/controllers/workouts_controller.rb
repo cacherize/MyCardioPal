81dcf779fb991fa93b5dbb23c1c2933279e6b9a9
cacherize <web.developing.maniac@gmail.com>
2014-07-19 01:06:59 -0500

    removed raise

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -7,7 +7,6 @@ class WorkoutsController < ApplicationController
     @workout = Workout.new(params[:workout])
 
     respond_to do |format|
-      raise @workout.inspect
       if @workout.save
         format.html{redirect_to @workout, notice: 'Success! Added workout!'}
       else
