07362ba3e9a4cdda065e1c776f9246411797c432
cacherize <web.developing.maniac@gmail.com>
2014-07-19 16:48:08 -0500

    added validations for date and time

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,7 @@
 class Workout < ActiveRecord::Base
   attr_accessible :user_id, :activity_id, :time, :date, :distance, :incline, :note, :distance_units, :weight, :weight_units
+  validates :date, presence: {message: 'is required'}
+  validates :time, presence: {message: 'is required'}, numericality: {greater_than: 59, message: 'must at least 1 minute'}
 
   def time=(values)
     hours = values[:hours].to_i * 3600
