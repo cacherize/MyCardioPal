ec9700c924a9661a6817862197a561a3940c7bb9
cacherize <web.developing.maniac@gmail.com>
2014-07-18 23:26:44 -0500

    added workout create form and new/create actions in controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,18 @@
 class WorkoutsController < ApplicationController
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(params[:workout])
+
+    respond_to do |format|
+      raise @workout.inspect
+      if @workout.save
+        format.html{redirect_to @workout, notice: 'Success! Added workout!'}
+      else
+        format.html{render :new}
+      end
+    end
+  end
 end
