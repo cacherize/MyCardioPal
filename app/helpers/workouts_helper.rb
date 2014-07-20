cdd574873404b04afcaac41b115890eaa3d75032
cacherize <web.developing.maniac@gmail.com>
2014-07-19 20:47:40 -0500

    added helper method to set value of date field on workout form

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,22 @@
 module WorkoutsHelper
+  def date_field_value(workout, params)
+    if workout.new_record?
+      value = (params.blank? ? Date.today : params[:date])
+    else
+      value = (params.blank? ? workout.date : params[:date])
+    end
+
+    unless value.is_a? Date
+      begin
+        value = Date.strptime(value, "%m %d %Y")
+      rescue ArgumentError => e
+        value = nil
+      else
+        value = value.strftime("%-m-%-d-%Y")
+      end
+    else
+      value = value.strftime("%-m-%-d-%Y")
+    end
+    return value
+  end
 end
