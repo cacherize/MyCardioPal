2c3efdfd08837278104d9e3d11a41a734f6ec7f4
cacherize <web.developing.maniac@gmail.com>
2014-07-20 19:36:29 -0500

    worked on validations for workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,17 +1,21 @@
 class Workout < ActiveRecord::Base
   attr_accessible :user_id, :activity_id, :time, :date, :distance, :incline, :note, :distance_units, :weight, :weight_units
-  validates :date, presence: {message: 'is required'}
-  validates :time, presence: {message: 'is required'}, numericality: {greater_than: 59, message: 'must at least 1 minute'}
+
+  #***** Validations *****#
+  validates_presence_of [:date, :time, :weight], message: 'is required'
+  validates :time, numericality: {greater_than: 59, message: 'must at least 1 minute'}, if: lambda{self.time.present?}
+  validates :weight, numericality: {greater_than: 1, message: "doesn't meet minimum allowed value"}, if: lambda{self.weight.present?}
   validate :date_class, if: lambda{self.date.present?}
 
   def date_class
     errors.add(:date, 'must be a valid format (ex: mm-dd-yyyy or mm/dd/yyyy)') unless self.date.is_a? Date
   end
 
-  def time=(values)
-    hours = values[:hours].to_i * 3600
-    minutes = values[:minutes].to_i * 60
-    seconds = values[:seconds].to_i
+  def time=(args)
+    return if args.values.all?{|val| val.blank?}
+    hours = args[:hours].to_i * 3600
+    minutes = args[:minutes].to_i * 60
+    seconds = args[:seconds].to_i
     self[:time] = hours + minutes + seconds
   end
 
