797784057670060778651a634739c6ff22a25220
cacherize <web.developing.maniac@gmail.com>
2014-07-24 21:30:42 -0500

    added presence validations for activity model attributes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ActiveRecord::Base
   attr_accessible :name, :met, :description, :position, :short_name
+  validates_presence_of [:name, :description, :short_name, :met], message: "is required"
 end
