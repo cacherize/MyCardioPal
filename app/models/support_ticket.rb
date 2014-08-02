98e32a65ad96be83fa6e8188cfdbb1d99e5520b4
cacherize <web.developing.maniac@gmail.com>
2014-08-02 17:55:37 -0500

    added validations for support tickets

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -1,3 +1,7 @@
 class SupportTicket < ActiveRecord::Base
   attr_accessible :full_name, :email, :reason, :other_reason, :message, :stickied, :archive
+  validates :full_name, presence: true
+  validates :email, presence: true
+  validates :reason, presence: true
+  validates :message, presence: true, length: {minimum: 10, maximum: 1500, if: lambda{self.message.present?}}
 end
