1808d94cc25ea5e1f49ae2a5cb7115368f4e65ea
cacherize <web.developing.maniac@gmail.com>
2014-08-02 15:45:35 -0500

    set up new and create action on support tickets controller

--- app/controllers/support_tickets_controller.rb
+++ app/controllers/support_tickets_controller.rb
@@ -3,8 +3,18 @@ class SupportTicketsController < ApplicationController
   end
 
   def new
+    @support_ticket = SupportTicket.new
   end
 
   def create
+    @support_ticket = SupportTicket.new(params[:support_ticket])
+    
+    respond_to do |format|
+      if @support_ticket.save
+        format.html{redirect_to root_path, notice: "Successfully submitted a support ticket. Please wait 24-48 hours for a response."}
+      else
+        format.html{render :new}
+      end
+    end
   end
 end
