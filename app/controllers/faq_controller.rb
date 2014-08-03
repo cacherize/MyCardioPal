f361386c22de47d5e63973ed433f90a4aef54ddb
cacherize <web.developing.maniac@gmail.com>
2014-08-03 02:01:55 -0500

    added index, create, and update action methods to faq controller

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,4 +1,12 @@
 class FaqController < ApplicationController
   def index
+    @faqs = Faq.all
+    @grouped_faqs = @faqs.in_groups(3, false)
+  end
+
+  def create
+  end
+
+  def update
   end
 end
