bcdf994b4f393a69a97c16e595be8c44aed765d1
cacherize <web.developing.maniac@gmail.com>
2014-08-03 02:51:04 -0500

    added Faq validations

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,4 +1,13 @@
 class Faq < ActiveRecord::Base
   attr_accessible :question, :answer, :position
   default_scope order('faqs.archived_at DESC, faqs.position ASC')
+
+  validates_presence_of :question, :answer
+  validate :is_a_question
+
+  def is_a_question
+    if self.question.present?
+      errors.add(:question, "must end in a question mark") unless self.question.last == "?"
+    end
+  end
 end
