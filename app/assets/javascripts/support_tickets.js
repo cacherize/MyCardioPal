e4ec98ebc73504a6f35a5a0bd514bca6b0ad5d03
cacherize <web.developing.maniac@gmail.com>
2014-08-02 19:09:33 -0500

    highlight other reason field if causing validation error

--- app/assets/javascripts/support_tickets.js
+++ app/assets/javascripts/support_tickets.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   if (!$("#support_ticket_reason_other").is(':checked')) {
     $("#otherReasonField").hide();
+  } else if ($("#support_ticket_other_reason").parent().hasClass('field_with_errors')) {
+   $("#otherReasonField").css('border-color', '#ff3131')
   }
 });
 
