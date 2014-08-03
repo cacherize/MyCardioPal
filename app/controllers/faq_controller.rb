67e240101eec7729469c3597d71f0c9e0d39f1a5
cacherize <web.developing.maniac@gmail.com>
2014-08-03 02:51:19 -0500

    set up create action for Faqs

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -2,9 +2,21 @@ class FaqController < ApplicationController
   def index
     @faqs = Faq.all
     @grouped_faqs = @faqs.in_groups(3, false)
+    @new_faq = Faq.new
   end
 
   def create
+    @new_faq = Faq.new(params[:faq])
+
+    respond_to do |format|
+      if @new_faq.save
+        format.html{redirect_to faq_index_path, notice: 'Success! Added FAQ!'}
+      else
+        @faqs = Faq.all
+        @grouped_faqs = @faqs.in_groups(3, false)
+        format.html{render :index}
+      end
+    end
   end
 
   def update
