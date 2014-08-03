8320620d4af8376189b6d5e80f1678799464a10f
cacherize <web.developing.maniac@gmail.com>
2014-08-03 12:32:59 -0500

    added archiving and unarchiving methods for FAQs

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,5 +1,5 @@
 class Faq < ActiveRecord::Base
-  attr_accessible :question, :answer, :position
+  attr_accessible :question, :answer, :position, :archive
   default_scope order('faqs.archived_at DESC, faqs.position ASC')
 
   validates_presence_of :question, :answer
@@ -11,6 +11,22 @@ class Faq < ActiveRecord::Base
     if self.question.present?
       errors.add(:question, "must end in a question mark") unless self.question.last == "?"
     end
+  end
+
+  def archive=(boolean)
+    if boolean == "true"
+      self.archive!
+    else
+      self.unarchive!
+    end
+  end
+
+  def archive!
+    self.update_attribute(:archived_at, DateTime.now)
+  end
+
+  def unarchive!
+    self.update_attribute(:archived_at, nil)
   end
 
   def archived?
