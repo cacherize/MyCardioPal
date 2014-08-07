4a6eba62122888568911462fa47f8c6c8ec366c1
cacherize <web.developing.maniac@gmail.com>
2014-08-06 22:43:29 -0500

    set config options on datepicker for workout date field

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
 });
 
 function loadWorkoutForm(){
-  $("#workout_date").datepicker();
+  $("#workout_date").datepicker({
+    dateFormat: "m-d-yy",
+    prevText: '\u2190',
+    nextText: '\u2192',
+    beforeShow:function( input, inst )
+      {
+          var dp = $(inst.dpDiv);
+          var offset = $(input).outerWidth(false) - dp.outerWidth(false);
+          dp.css('margin-left', offset + 1);
+      }
+  });
 }
