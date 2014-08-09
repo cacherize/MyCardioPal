0f6b1a43fb8ce77dc2efbfd5373ea47d425037a9
cacherize <web.developing.maniac@gmail.com>
2014-08-08 23:30:03 -0500

    added helper method for loading anim html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def loader_anim
+    '<div class="loader">
+        <div class="dot dot1">></div>
+        <div class="dot dot2">></div>
+        <div class="dot dot3">></div>
+        <div class="dot dot4">></div>
+    </div>'
+  end
 end
